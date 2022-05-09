2430e9d71f4b5c3481c06bc44e7827bc10bf09fd
DylanGaff <102431405+DylanGaff@users.noreply.github.com>
2022-05-09 19:50:37 -0400

    added text to Javascript_file.js

--- Basic-Javascript-Projects/Project1-expressions-alert/JS/Javascript_file.js
+++ Basic-Javascript-Projects/Project1-expressions-alert/JS/Javascript_file.js
@@ -1,9 +1,12 @@
-alert('Hello, World');
+var Family = 'The Gaffeys', Dad = 'Bob', Mom = 'Rachael', Brother = 'Dakotta', Wife = 'Sam'; //creating variables to put my family in to
+document.write(Wife); //writing the 'Wife' variable on the webpage
 
-window.alert('Hello, World2');
+3 * 7; //creating an expression to be written out
+document.write(3 * 7); //writing out the expression result on the webpage
 
-window.alert(B)
+window.alert(Family); //using a pop-up alert box to display the 'Family' variable
 
-
-
-
+document.write("Success is not final," //writing out a quote by winston churchill
++ "Failure is not fatal," //usiing the + operator to concatenate the variables together
++ "It is the courage to continue that counts"
++ "-Winston S Churhill");
